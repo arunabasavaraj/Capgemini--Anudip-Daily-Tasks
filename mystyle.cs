d79fc525e6800bcc7840b6d885c42be0d8e3e94f
aruna <69453723+arunabasavaraj@users.noreply.github.com>
2021-08-25 03:16:48 -0400

    Add files via upload

---
+++ mystyle.cs
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel="stylesheet" href="mystyle.css">
+</head>
+<body>
+
+<h1>This is a heading</h1>
+<h2>This is a heading</h2>
+<h3>This is a heading</h3>
+<h4>This is a heading</h4>
+
+
+<p>This is a paragraph.</p>
+
+</body>
+</html>
